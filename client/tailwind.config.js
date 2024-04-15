f218e7a3e6173474b445ab39fa81e035bc5cce17
iso-bravo <isolettebravo@gmail.com>
2024-04-14 21:58:01 -0700

    Client init

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx,mdx}",
+  "./src/*.{html,js,ts,jsx,tsx,mdx}",
+  "./index.html",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
